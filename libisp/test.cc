c03fd84609d094b474be64b8919d20d5938283df
Krister Joas <krister@joas.jp>
2020-04-30 07:34:11 +0900

    Test: Add test of return value of 'intern'

--- libisp/test.cc
+++ libisp/test.cc
@@ -3,8 +3,22 @@
 #include <doctest/doctest.h>
 
 #include "lisp.hh"
+#include "alloc.hh"
 
 TEST_CASE("Create lisp object")
 {
   lisp::lisp lisp;
+  SUBCASE("intern should return the same object for the same string")
+  {
+    auto hello0 = lisp.a().intern("hello");
+    auto hello1 = lisp.a().intern("hello");
+    CHECK(hello0 == hello1);
+  }
+  SUBCASE("intern from two different lisp objects should be different")
+  {
+    lisp::lisp lisp1;
+    auto hello0 = lisp.a().intern("hello");
+    auto hello1 = lisp1.a().intern("hello");
+    CHECK(hello0 != hello1);
+  }
 }
