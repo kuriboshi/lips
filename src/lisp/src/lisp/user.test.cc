d3c0a6cb3e725d28c4930e90bdef2348aa2233f0
Krister Joas <krister@joas.jp>
2021-07-27 16:26:53 +0900

    Fixed de/df test

--- src/lisp/src/lisp/user.test.cc
+++ src/lisp/src/lisp/user.test.cc
@@ -13,23 +13,22 @@ TEST_CASE("User defined functions")
   lisp l;
   current c(l);
 
-  SUBCASE("lambda/nlambda")
+  SUBCASE("de/df")
   {
     auto a = mkatom("a");
     auto b = mkatom("b");
-    auto lam = lambda(l, mklist(mkatom("a")), mklist(mkatom("a")));
-    auto nlam = nlambda(l, mklist(mkatom("a")), mklist(mkatom("a")));
-    set(l, mkatom("a"), mkatom("b"));
-    apply(l, lam, cons(a, NIL));
-    auto r0 = cons(l, eval(l, mklist(nlam, a)), eval(l, mklist(lam, a)));
+    auto nlam = df(l, mkatom("f0"), mklist(mkatom("a")), mklist(mkatom("a")));
+    auto lam = de(l, mkatom("f1"), mklist(mkatom("a")), mklist(mkatom("a")));
+    set(l, a, b);
+    auto r0 = eval(l, "(cons (f0 a) (f1 a))");
     CHECK(car(r0) == a);
     CHECK(cdr(r0) == b);
   }
+
   SUBCASE("defineq")
   {
     auto f0 = mklist(mklist(mkatom("f0"), lambda(mklist(mkatom("a")), mklist(mkatom("a")))),
       mklist(mkatom("f1"), lambda(mklist(mkatom("b")), mklist(mkatom("b")))));
-    print(f0);
     auto r0 = defineq(f0);
     REQUIRE(type_of(r0) == type::CONS);
     CHECK(eq(r0->car(), mkatom("f0")));
