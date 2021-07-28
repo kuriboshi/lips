77260dd07659c15e1f5e8720949e024b8390aff0
Krister Joas <krister@joas.jp>
2021-07-28 16:04:28 +0900

    Use defineq in pretty.lisp

--- lisp/pretty.lisp
+++ lisp/pretty.lisp
@@ -10,146 +10,151 @@
 #
 (setq %rpars nil)
 
-(df pp funs
-    (mapc funs (lambda (name)
-		 (terpri)
-	      	 (if (memb (typeof (eval name)) '(subr fsubr))
-                     (cpprint name)
-                     (pprint (list name (getrep (eval name)))))))
-    funs)
+(defineq
+  (pp
+   (nlambda funs
+            (mapc funs (lambda (name)
+                         (terpri)
+                         (if (memb (typeof (eval name)) '(subr fsubr))
+                             (cpprint name)
+                           (pprint (list name (getrep (eval name)))))))
+            funs))
 
-(de pprint (exp)
- (pp1 exp 0)
-    (terpri)
-    nil)
+  (pprint
+   (lambda (exp)
+     (pp1 exp 0)
+     (terpri)
+     nil))
 
-(de pp1 (exp indent super)
-    (cond
+  (pp1
+   (lambda (exp indent super)
+     (cond
       ((listp exp)
-	(cond
-	  ((and (numberp %rpars)
-                (greaterp (depth exp)
-			  %rpars)
-		(not super))
-	    (prin1 "[")
-	    (ppl exp indent t)
-	    (prin1 "]"))
-	  (t (prin1 "(")
-	     (ppl exp indent super))))
+       (cond
+        ((and (numberp %rpars)
+              (greaterp (depth exp)
+                        %rpars)
+              (not super))
+         (prin1 "[")
+         (ppl exp indent t)
+         (prin1 "]"))
+        (t (prin1 "(")
+           (ppl exp indent super))))
       (t (prin2 exp)))
-    nil)
+     nil))
 
-(de ppl
-  (exp indent super)
-    (cond
+  (ppl
+   (lambda (exp indent super)
+     (cond
       ((atom (car exp))
-	(ppl1 exp indent super))
+       (ppl1 exp indent super))
       (t (setq indent (add1 indent))
-	 (pp1 (car exp)
-	       indent
-	       (and super (not (cdr exp))))
-	 (setq exp (cdr exp))
-	 (and exp (progn (ppnl indent)
-			 (pptail exp indent super)))))
-    (and (not super)
-	 (prin1 ")"))
-    nil)
+         (pp1 (car exp)
+              indent
+              (and super (not (cdr exp))))
+         (setq exp (cdr exp))
+         (and exp (progn (ppnl indent)
+                         (pptail exp indent super)))))
+     (and (not super)
+          (prin1 ")"))
+     nil))
 
-(de ppl1
-  (exp indent super tmp)
-    (cond
+  (ppl1
+   (lambda (exp indent super tmp)
+     (cond
       ((memb (car exp)
-	     (quote (lambda nlambda while)))
-	(prin2 (car exp))
-	(pplam (cdr exp)
-		indent super))
+             (quote (lambda nlambda while)))
+       (prin2 (car exp))
+       (pplam (cdr exp)
+              indent super))
       ((memb (car exp)
-	     (quote (cond closure)))
-	(prin2 (car exp))
-	(ppcond (cdr exp)
-		 indent super))
-      (t (ppfun exp indent super))))
+             (quote (cond closure)))
+       (prin2 (car exp))
+       (ppcond (cdr exp)
+               indent super))
+      (t (ppfun exp indent super)))))
 
-(de ppcond
-  (exp indent)
-    (ppnl (setq indent (add1 indent)))
-    (pptail exp indent super))
+  (ppcond
+   (lambda (exp indent)
+     (ppnl (setq indent (add1 indent)))
+     (pptail exp indent super)))
 
-(de pplam
-  (exp indent)
-    (prin1 " ")
-    (cond
+  (pplam
+   (lambda (exp indent)
+     (prin1 " ")
+     (cond
       ((car exp)
-	(prin2 (car exp)))
+       (prin2 (car exp)))
       (t (prin1 "()")))
-    (ppnl (setq indent (add1 indent)))
-    (pptail (cdr exp)
-	     indent super))
+     (ppnl (setq indent (add1 indent)))
+     (pptail (cdr exp)
+             indent super)))
 
-(de ppfun
-  (exp indent)
-    (prin2 (car exp))
-    (and (cdr exp)
-	 (prin1 " "))
-    (pptail (cdr exp)
-	     (+ indent (symlen (car exp))
-		   2)
-	     super))
+  (ppfun
+   (lambda (exp indent)
+     (prin2 (car exp))
+     (and (cdr exp)
+          (prin1 " "))
+     (pptail (cdr exp)
+             (+ indent (symlen (car exp))
+                2)
+             super)))
 
-(de pptail
-  (exp indent super)
-    (cond
+  (pptail
+   (lambda (exp indent super)
+     (cond
       ((null exp))
       ((and (litatom (car exp))
-	    (lessp (length (cadr exp))
-		   3)
-	    (lessp (length exp)
-		   3))
-	(setq indent (+ indent (symlen (car exp))
-			   1))
-	(prin2 (car exp))
-	(and (setq exp (cdr exp))
-	     (prin1 " "))))
-    (pptail1 exp indent super))
+            (lessp (length (cadr exp))
+                   3)
+            (lessp (length exp)
+                   3))
+       (setq indent (+ indent (symlen (car exp))
+                       1))
+       (prin2 (car exp))
+       (and (setq exp (cdr exp))
+            (prin1 " "))))
+     (pptail1 exp indent super)))
 
-(de pptail1
-  (exp indent super indent2)
-  (setq indent2 indent)
-  (while exp
-    (pp1 (car exp)
-         indent2
-         (and super (not (cdr exp))))
-    (and (cdr exp)
-         (cond
-          ((and (litatom (car exp))
-                (setq indent2
-                      (+ indent2 (symlen (car exp)) 1))
-                (if (greaterp indent2 60)
-                    (lessp (+ indent2
-                              (if (litatom (cadr exp))
-                                  (symlen (cadr exp))
-                                0)) 79)
-                  t))
-           (prin1 " "))
-          (t (ppnl indent)
-             (setq indent2 indent))))
-    (setq exp (cdr exp))))
+  (pptail1
+   (lambda (exp indent super indent2)
+     (setq indent2 indent)
+     (while exp
+       (pp1 (car exp)
+            indent2
+            (and super (not (cdr exp))))
+       (and (cdr exp)
+            (cond
+             ((and (litatom (car exp))
+                   (setq indent2
+                         (+ indent2 (symlen (car exp)) 1))
+                   (if (greaterp indent2 60)
+                       (lessp (+ indent2
+                                 (if (litatom (cadr exp))
+                                     (symlen (cadr exp))
+                                   0)) 79)
+                     t))
+              (prin1 " "))
+             (t (ppnl indent)
+                (setq indent2 indent))))
+       (setq exp (cdr exp)))))
 
-(de ppnl
-  (indent)
-    (terpri)
-    (spaces indent))
+  (ppnl
+   (lambda (indent)
+     (terpri)
+     (spaces indent)))
 
-(de symlen (s)
-  (strlen (symstr s)))
+  (symlen
+   (lambda (s)
+     (strlen (symstr s))))
 
-(de depth
-  (l)
-    (cond
+  (depth
+   (lambda (l)
+     (cond
       ((nlistp l)
-	0)
+       0)
       ((null l)
-	0)
+       0)
       ((null (cdr l))
-	(add1 (depth (car l))))
-      (t (depth (cdr l)))))
+       (add1 (depth (car l))))
+      (t (depth (cdr l)))))))
