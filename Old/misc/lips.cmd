f6734065de6241b72e35fcf2dfc11479cf2b33a1
Krister Joas <krister@joas.jp>
2021-10-30 18:12:48 +0900

    Remove old TOPS-20 related build and link scripts

--- Old/misc/lips.cmd
+++
@@ -1,6 +0,0 @@
-! %M% -- links lips on a TOPS20 system
-! %Z%%M% %E%
-LOAD ALLOC, ARITH, DEBUG, EVAL, -
-     FILE, LOGIC, LOW, UNIX, -
-     MAIN, MAP, PRED, PRIM, PROP, -
-     READ, STRING, TERM, TOP, USER
