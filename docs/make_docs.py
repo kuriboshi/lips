947425a340c2153d1291dfaae97fe179d9645347
Krister Joas <krister@joas.jp>
2023-12-23 19:05:44 +0900

    docs: Refine markdown document generation

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -11,19 +11,6 @@ class State(Enum):
     FILE = 1
     HEADER = 2
 
-class Description(object):
-    def __init__(self):
-        self.data = []
-
-    def add(self, line):
-        self.data.append(line)
-
-    def print(self):
-        if self.data:
-            print('')
-            [print(i) for i in self.data]
-        self.data = []
-
 class Signature(object):
     def __init__(self):
         self.data = []
@@ -36,57 +23,85 @@ class Signature(object):
     def deactivate(self):
         self.active = False
 
-    def print(self):
+    def print(self, output, prefix):
         if self.data:
-            print('\n```c++')
-            for i in self.data:
-                print(i)
-            print('```')
+            print(f'\n{prefix}```cpp', file=output)
+            [print(f'{prefix}{i}', file=output) for i in self.data]
+            print(f'{prefix}```', file=output)
         self.data = []
         self.active = True
 
-def print_doc(signature, description):
-    if description:
-        [print(i) for i in description]
-        description = []
-    if signature:
-        print('\n```c++')
-        [print(i) for i in signature]
-        print('```')
-        signature = []
-    return (signature, description)
+class Description(object):
+    def __init__(self):
+        self.data = []
+        self.lisp_signature = None
+        self.cpp_signature = Signature()
+
+    def add(self, line):
+        self.data.append(line)
+
+    def lisp(self, line):
+        self.lisp_signature = line
+
+    def cpp(self, line):
+        self.cpp_signature.add(line)
+
+    def cpp_deactivate(self, line):
+        self.cpp_signature.deactivate()
+
+    def print(self, output):
+        if self.lisp_signature:
+            print('', file=output)
+            print(f'> {self.lisp_signature}', file=output)
+            self.cpp_signature.print(output, '')
+        else:
+            self.cpp_signature.print(output, '> ')
+        if self.data:
+            print('', file=output)
+            # self.data[0] = prefix + self.data[0]
+            [print(i, file=output) for i in self.data]
+        self.data = []
+        self.lisp_signature = None
 
 def main(dir):
     p = Path(dir)
     for file in p.iterdir():
         if file.suffix == '.hh':
             state = State.NONE
-            signature = Signature()
             description = Description()
+            output = None
             for line in fileinput.input(files=file):
                 if re.search('^/// @file', line):
                     state = State.HEADER
+                    output = open(Path('docs') / file.with_suffix('.md').name, 'w', encoding='utf-8')
                     continue
                 if state == State.NONE:
                     continue
                 # A @brief command starts a new function description.
                 m = re.match('^/// @brief (.*)', line)
                 if m:
-                    description.print()
-                    signature.print()
-                    description.add(f'> {m.group(1)}')
+                    description.print(output)
+                    description.add(m.group(1))
                     state = State.FILE
+                    continue
+                m = re.match('^/// @lisp{(\(.*\)),(.*)}', line)
+                if m:
+                    description.lisp(f'`{m.group(1)}` (_{m.group(2)}_)')
+                    state = State.FILE
+                    continue
+                m = re.match('^/// (.*)}', line)
+                if m:
+                    description.add(m.group(1))
                     continue
                 # If we're in the header part of the file just print
                 # the documentation comments.
                 m = re.match('^/// ?(.*)', line)
                 if m and state == State.HEADER:
-                    print(m.group(1))
                     continue
                 if state != State.FILE:
                     continue
                 # Process the @tparam or @param commands.
-                m = re.match('^/// @t?param ([a-zA-Z]*) (.*)', line)
+                m = re.match('^/// @t?param ([a-zA-Z0-9]*) (.*)', line)
                 if m:
                     description.add(f'- _{m.group(1)}_ {m.group(2)}')
                     continue
@@ -103,23 +118,24 @@ def main(dir):
                 # Start processing the signature of a C++ function.
                 m = re.match('^([^/].*){', line)
                 if m:
-                    signature.add(m.group(1).strip())
+                    description.cpp(m.group(1).strip())
                     continue
                 # When we see a blank line, any curly brace or # ends the signature.
                 m = re.match('^$', line)
                 if m:
-                    signature.deactivate()
+                    description.cpp_deactivate(line)
                     continue
                 m = re.match('^[{}#].*', line)
                 if m:
-                    signature.deactivate()
+                    description.cpp_deactivate(line)
                     continue
                 m = re.match('^([^/].*)', line)
                 if m:
-                    signature.add(m.group(1).strip())
+                    description.cpp(m.group(1).strip())
                     continue
-            description.print()
-            signature.print()
-
+            description.print(output)
+            if output:
+                output.close()
+                output = None
 
 main('src/lisp')
