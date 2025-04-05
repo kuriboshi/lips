1c7f56fac9f5d084e380f1c16a22d90f238e9bbd
Krister Joas <krister@joas.jp>
2025-04-05 10:35:01 +0900

    copyright: Update

--- src/lisp/run.hh
+++ src/lisp/run.hh
@@ -1,6 +1,6 @@
 //
 // Lips, lisp shell.
-// Copyright 2021-2024 Krister Joas
+// Copyright 2021-2025 Krister Joas
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
